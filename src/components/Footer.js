eda244e3f5a46fdab9cca05ba4d008c6cf4d70a5
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-04-12 17:18:08 +0430

    footer is done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import '../styles/footer.css';
+import githubIcon from '../icons/ic_github.svg';
+import linkedIn from '../icons/ic_linkedin.svg';
+import twitter from '../icons/ic_twitter.svg';
 
 function Footer() {
   return (
-    <footer>
-      <h2>Hi, I am footer</h2>
+    <footer className="footer">
+      <h2>
+        Ali-Safari
+        <span className="copy">&copy;</span>
+      </h2>
+      <ul className="social-media">
+        <li>
+          <a href="https://twitter.com/qurban_safari" title="Twitter">
+            <img src={twitter} alt="twitter_logo" />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://github.com/Ali-0111" title="Open GitHub">
+            <img src={githubIcon} alt="twitter_logo" />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.linkedin.com/in/ali-safari-695214202/" title="LinkedIn">
+            <img src={linkedIn} alt="LinkedIn" />
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
