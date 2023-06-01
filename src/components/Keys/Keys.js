0a749a60343eef9ac29f1fa719e0d9a395be17ed
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-06-01 15:05:43 +0430

    Codes Refactored

--- src/components/Keys/Keys.js
+++ src/components/Keys/Keys.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import RowOfButtons from './RowOfButtons';
 import '../../styles/keys.css';
 
-function Keys() {
+function Keys({ updateState }) {
+  const btnsInnerText = [
+    ['row row-1', 'AC', '+/-', '%', '÷'],
+    ['row row-2', '7', '8', '9', 'x'],
+    ['row row-3', '4', '5', '6', '-'],
+    ['row row-4', '1', '2', '3', '+'],
+    ['row row-5', '0', '.', '='],
+  ];
   return (
     <div className="keys-container">
-      <RowOfButtons info={['row row-1', 'AC', '+/-', '%', '÷']} />
-      <RowOfButtons info={['row row-2', '7', '8', '9', 'x']} />
-      <RowOfButtons info={['row row-3', '4', '5', '6', '-']} />
-      <RowOfButtons info={['row row-4', '1', '2', '3', '+']} />
-      <RowOfButtons info={['row row-5', '0', '.', '=']} />
+      {
+        btnsInnerText.map((info, i) => (
+          <RowOfButtons
+            key={`row${i + 1}`}
+            info={info}
+            updateState={updateState}
+          />
+        ))
+      }
     </div>
   );
 }
+
+Keys.propTypes = {
+  updateState: PropTypes.func.isRequired,
+};
 
 export default Keys;
