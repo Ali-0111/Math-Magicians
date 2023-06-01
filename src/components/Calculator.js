d644d22247bf50985166c4c596ac401894b8f88c
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-06-01 15:04:57 +0430

    State added

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import getNotNull from '../util/helper/notNull';
 import Keys from './Keys/Keys';
+import calculate from '../logic/calculate';
 import '../styles/Frame.css';
 
 function Calculator() {
+  const dataSample = {
+    total: null,
+    operation: null,
+    next: null,
+  };
+  const [calcData, setCalcData] = useState({ ...dataSample });
+
+  const updateState = (btnText) => {
+    const calculatedObject = calculate(calcData, btnText);
+    setCalcData(calculatedObject);
+  };
+
+  const screenText = getNotNull(calcData);
+
   return (
     <div className="calc-frame">
       <div className="screen">
-        <h2 className="output">0</h2>
+        <h2 className="output">{screenText}</h2>
       </div>
-      <Keys />
+      <Keys updateState={updateState} />
     </div>
   );
 }
