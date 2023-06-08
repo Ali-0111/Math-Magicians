174cf9eb57a3236aa745ea987437fc71b3593255
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-06-08 12:10:58 +0430

    Header Implementation

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.css';
+
+function Header() {
+  return (
+    <header className="main-head">
+      <h1 className="title">Math Magicians</h1>
+      <ul className="navs">
+        <li><Link className="link" to="/">Home</Link></li>
+        <li><Link className="link" to="calc">Calculator</Link></li>
+        <li><Link className="link" to="quote">Quote</Link></li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
